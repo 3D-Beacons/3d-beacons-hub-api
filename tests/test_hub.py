df852eefdc29423971fef4671262cefc5dcae800
Sreenath S. Nair <sreenath@ebi.ac.uk>
2020-10-08 17:16:13 +0100

    Added test cases for UniProt, test utils, github actions

--- tests/test_hub.py
+++ tests/test_hub.py
@@ -1,4 +1,7 @@
-from fastapi.testclient import TestClient
+import asyncio
+
+from async_asgi_testclient import TestClient
+import pytest
 from starlette import status
 
 from app.app import app
@@ -7,12 +10,14 @@ client = TestClient(app)
 
 
 # Using 'with TestClient' to test the startup and shutdown events
-def test_get_uniprot_api_no_range(valid_uniprot):
-    with TestClient(app) as client:
-        response = client.get(f"/uniprot/{valid_uniprot}.json")
+@pytest.mark.asyncio
+async def test_get_uniprot_api(
+    mocker, valid_uniprot, valid_uniprot_structures, services
+):
+    async with TestClient(app) as client:
+        future = asyncio.Future()
+        future.set_result(valid_uniprot_structures)
+        mocker.patch("app.uniprot.uniprot.get_services", return_value=services)
+        mocker.patch("app.uniprot.uniprot.send_async_requests", return_value=future)
+        response = await client.get(f"/uniprot/{valid_uniprot}.json")
         assert response.status_code == status.HTTP_200_OK
-
-
-def test_get_uniprot_api_with_range(valid_uniprot):
-    response = client.get(f"/uniprot/{valid_uniprot}.json?range=10-50")
-    assert response.status_code == status.HTTP_200_OK
