f25d080dfe4b056c77cb1518f6167e8c8f54a455
Sreenath S. Nair <sreenath@ebi.ac.uk>
2020-11-12 18:02:20 +0000

    PDBE-4273: Added uniprot summary call

--- tests/conftest.py
+++ tests/conftest.py
@@ -22,6 +22,13 @@ def valid_uniprot_structures():
 
 
 @pytest.fixture(scope="session")
+def valid_uniprot_structures_summary():
+    valid_structures = []
+    valid_structures.append(StubResponse(status_code=200, stub_for="uniprot_summary"))
+    return valid_structures
+
+
+@pytest.fixture(scope="session")
 def services():
     with open("app/config/data.json") as fp:
         data = json.load(fp)
--- tests/test_hub.py
+++ tests/test_hub.py
@@ -21,3 +21,15 @@ async def test_get_uniprot_api(
         mocker.patch("app.uniprot.uniprot.send_async_requests", return_value=future)
         response = await client.get(f"/uniprot/{valid_uniprot}.json")
         assert response.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.asyncio
+async def test_get_uniprot_summary_api(
+    mocker, valid_uniprot, valid_uniprot_structures_summary, services
+):
+    future = asyncio.Future()
+    future.set_result(valid_uniprot_structures_summary)
+    mocker.patch("app.uniprot.uniprot.get_services", return_value=services)
+    mocker.patch("app.uniprot.uniprot.send_async_requests", return_value=future)
+    response = await client.get(f"/uniprot/summary/{valid_uniprot}.json")
+    assert response.status_code == status.HTTP_200_OK
