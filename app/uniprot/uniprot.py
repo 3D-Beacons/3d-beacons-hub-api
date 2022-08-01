3de2883bd75efeb9e529a53893f7ea63b22728fb
sreenath <sreenath@ebi.ac.uk>
2022-08-01 11:41:32 +0100

    Updated error response logging

--- app/uniprot/uniprot.py
+++ app/uniprot/uniprot.py
@@ -100,10 +100,11 @@ async def get_uniprot_summary(
             UniprotEntry(**item["uniprot_entry"])
             final_structures.extend(item["structures"])
         except pydantic.error_wrappers.ValidationError:
-            provider = item["structures"][0]["provider"]
-            logger.debug(
-                f"Invalid response from provider {provider} for entry {qualifier}"
-            )
+            provider = item["structures"][0].get("provider")
+            if provider:
+                logger.warning(
+                    f"{provider} returned an erroneous response for {qualifier}"
+                )
         except Exception:
             final_structures = None
 
@@ -187,10 +188,11 @@ async def get_uniprot(
             UniprotEntry(**item["uniprot_entry"])
             final_structures.extend(item["structures"])
         except pydantic.error_wrappers.ValidationError:
-            provider = item["structures"][0]["provider"]
-            logger.debug(
-                f"Invalid response from provider {provider} for entry {qualifier}"
-            )
+            provider = item["structures"][0].get("provider")
+            if provider:
+                logger.warning(
+                    f"{provider} returned an erroneous response for {qualifier}"
+                )
         except Exception:
             final_structures = None
 
